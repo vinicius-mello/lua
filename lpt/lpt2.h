192901321f4cfa2b1264ca8f7960f53ed2f6d9ef
Vinicius Mello <vinicius.moreira.mello@gmail.com>
2026-02-08 15:55:48 -0300

    lpt.h generic

--- lpt/lpt2.h
+++ lpt/lpt2.h
@@ -33,26 +33,10 @@ typedef union lpt2_u lpt2;
 struct lpt2_tree_s;
 typedef struct lpt2_tree_s lpt2_tree;
 
-void lpt2_init(lpt2 *code, uint8_t perm);
-uint64_t lpt2_tointeger(lpt2 code);
-lpt2 lpt2_frominteger(uint64_t code);
-int lpt2_level(lpt2 code);
-int lpt2_simplex_level(lpt2 code);
-int lpt2_orthant_level(lpt2 code);
-lpt2 lpt2_child(lpt2 code, int zo);
-bool lpt2_is_child0(lpt2 code); 
-lpt2 lpt2_parent(lpt2 code); 
-int lpt2_orientation(lpt2 code);
-int lpt2_neighbor_bd(lpt2 code, int i, lpt2 *r);
-bool lpt2_neighbor(lpt2 code, int i, lpt2 *r);
-void lpt2_simplex(lpt2 code, double *s);
-void lpt2_print_simplex(lpt2 code);
-lpt2_tree * lpt2_tree_new(size_t buckets);
-void lpt2_tree_free(lpt2_tree *tree);
-void lpt2_tree_print_stats(lpt2_tree *tree);
-void lpt2_tree_compat_bisect(lpt2_tree *tree, lpt2 code, void (*subdivided)(lpt2,void*), void *udata);
-bool lpt2_tree_is_leaf(lpt2_tree *tree, lpt2 code);
-void lpt2_tree_visit_leafs(lpt2_tree *tree, void (*visit)(lpt2,void*), void *udata);
-void lpt2_tree_search_all(lpt2_tree *tree, double * p, void (*visit)(lpt2,void*), void *udata);
+#define LPT(x) lpt2_##x
+#define lpt lpt2
+#include "lpt_inc.h"
+#undef lpt
+#undef LPT
 
 #endif // LPT2_H
--- lpt/lpt3.h
+++ lpt/lpt3.h
@@ -29,27 +29,10 @@ typedef struct lpt3_tree_s lpt3_tree;
 #define LPT3_DIM 3
 #define LPT3_MAX_ORTHANT_LEVEL 16
 
-
-void lpt3_init(lpt3 *code, uint8_t perm);
-uint64_t lpt3_tointeger(lpt3 code);
-lpt3 lpt3_frominteger(uint64_t code);
-int lpt3_level(lpt3 code);
-int lpt3_simplex_level(lpt3 code);
-int lpt3_orthant_level(lpt3 code);
-lpt3 lpt3_child(lpt3 code, int zo);
-bool lpt3_is_child0(lpt3 code); 
-lpt3 lpt3_parent(lpt3 code); 
-int lpt3_orientation(lpt3 code);
-int lpt3_neighbor_bd(lpt3 code, int i, lpt3 *r);
-bool lpt3_neighbor(lpt3 code, int i, lpt3 *r);
-void lpt3_simplex(lpt3 code, double *s);
-void lpt3_print_simplex(lpt3 code);
-lpt3_tree * lpt3_tree_new(size_t buckets);
-void lpt3_tree_free(lpt3_tree *tree);
-void lpt3_tree_print_stats(lpt3_tree *tree);
-void lpt3_tree_compat_bisect(lpt3_tree *tree, lpt3 code, void (*subdivided)(lpt3,void*), void *udata);
-bool lpt3_tree_is_leaf(lpt3_tree *tree, lpt3 code);
-void lpt3_tree_visit_leafs(lpt3_tree *tree, void (*visit)(lpt3,void*), void *udata);
-void lpt3_tree_search_all(lpt3_tree *tree, double * p, void (*visit)(lpt3,void*), void *udata);
+#define LPT(x) lpt3_##x
+#define lpt lpt3
+#include "lpt_inc.h"
+#undef lpt
+#undef LPT
 
 #endif // LPT3_H
